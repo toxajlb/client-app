f270042e68131e89cc487b7bcdac8988e797741c
Anton <toxajlb@yandex.ru>
2022-12-23 16:58:45 +0300

    Have been started frontend part of test project

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Index() {
+    return (
+        <>
+            <Navbar/>
+            <div className="center">
+                <h1>Добро пожаловать</h1>
+                <h3>Здесь собраны лучшие треки</h3>
+            </div>
+
+            <style jsx>
+                {`
+                    .center {
+                        margin-top: 150px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                `}
+            </style>
+        </>
+    )
+}
